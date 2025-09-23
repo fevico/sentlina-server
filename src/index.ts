90672918ca56375df4a8c9c751fb62406db98712
fevico <ajayifavour81@gmail.com>
2025-09-23 22:55:22 +0100

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.send("Server running fine!")
+})
+
+app.listen(() => {
+    console.log("Server started!")
+})
